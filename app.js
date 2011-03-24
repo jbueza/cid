b1ec105c358121103127d4ae2fc63b9df1627a82
jbueza <jbueza@gmail.com>
2011-03-24 11:12:56 -0700

    Remove extra configs

--- app.js
+++ app.js
@@ -8,15 +8,10 @@ var express = require('express')
 
 var app = module.exports = express.createServer();
 
-// Configuration
-
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  //app.use(express.static(__dirname + '/public'));
 });
 
 // Routes
